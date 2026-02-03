15b14587f77f8c2eb5dfbf30a034e889ad12aaae
httpluris7 <blancoabenzaluismanuel@gmail.com>
2026-02-03 16:55:15 +0100

    Update auth_drive.py

--- auth_drive.py
+++ auth_drive.py
@@ -3,9 +3,19 @@ import pickle
 
 SCOPES = ['https://www.googleapis.com/auth/drive']
 flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-creds = flow.run_console()
+
+auth_url, _ = flow.authorization_url(prompt='consent')
+
+print('\n1. Abre esta URL en tu navegador:\n')
+print(auth_url)
+print('\n2. Autoriza y copia la URL completa de la pagina final')
+print('   (aunque diga que no se puede conectar)\n')
+
+redirect_url = input('Pega aqui la URL completa: ')
+
+flow.fetch_token(authorization_response=redirect_url)
 
 with open('token.pickle', 'wb') as token:
-    pickle.dump(creds, token)
+    pickle.dump(flow.credentials, token)
 
-print('Token creado!')
+print('\nToken creado!')
