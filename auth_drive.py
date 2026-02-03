a47559e57c6e57e637a984df5f6112f7b0f00a47
httpluris7 <blancoabenzaluismanuel@gmail.com>
2026-02-03 16:58:36 +0100

    Update auth_drive.py

--- auth_drive.py
+++ auth_drive.py
@@ -6,10 +6,11 @@ flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
 
 auth_url, _ = flow.authorization_url(prompt='consent')
 
-print('\n1. Abre esta URL en tu navegador:\n')
-print(auth_url)
-print('\n2. Autoriza y copia la URL completa de la pagina final')
-print('   (aunque diga que no se puede conectar)\n')
+with open('url.txt', 'w') as f:
+    f.write(auth_url)
+
+print('URL guardada en url.txt')
+print('Ejecuta: cat url.txt')
 
 redirect_url = input('Pega aqui la URL completa: ')
 
@@ -18,4 +19,4 @@ flow.fetch_token(authorization_response=redirect_url)
 with open('token.pickle', 'wb') as token:
     pickle.dump(flow.credentials, token)
 
-print('\nToken creado!')
+print('Token creado!')
